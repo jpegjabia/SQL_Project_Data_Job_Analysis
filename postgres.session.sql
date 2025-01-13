fd12c575ce56038ddc047aa485892ac8a463bdac
jpegjabia <aobolbolychev@yandex.ru>
2025-01-13 15:52:11 +0300

    Last update, completed for now.

--- postgres.session.sql
+++ postgres.session.sql
@@ -83,26 +83,26 @@ ORDER BY avg_salary DESC
 LIMIT 25;
 
 /*
-5. Find the skills which are the most high paying and most demanded for the remote working data analyst.
+5. Find the skills which are the most high paying on average and most demanded 
+for the remote working data analyst.
 */
 
-WITH skills_demand AS (
+WITH jobs_popularity AS (
     SELECT
-        skills_job_dim.skill_id,
-        skills,
+        skills_dim.skill_id,
+        skills_dim.skills,
         COUNT(skills_job_dim.job_id) AS demand_count
     FROM job_postings_fact 
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE 
         job_title_short = 'Data Analyst'
-        AND salary_year_avg IS NOT NULL 
+        AND salary_year_avg IS NOT NULL
         AND job_work_from_home = True
-    GROUP BY skill_id
-), average_salary AS (
+    GROUP BY skills_dim.skill_id
+), jobs_salaries AS ( 
     SELECT
         skills_job_dim.skill_id,
-        skills,
         ROUND(AVG(salary_year_avg), 0) AS avg_salary
     FROM job_postings_fact 
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
@@ -111,13 +111,15 @@ WITH skills_demand AS (
         job_title_short = 'Data Analyst' 
         AND salary_year_avg IS NOT NULL 
         AND job_work_from_home = True
-    GROUP BY skill_id
+    GROUP BY skills_job_dim.skill_id
 )
-
 SELECT 
-    skills_demand.skill_id, 
-    skills_demand.skill,
+    jobs_popularity.skill_id,
+    jobs_popularity.skills,
     demand_count,
     avg_salary
-FROM skills_demand
-INNER JOIN average_salary ON skills_demand.skills_id = average_salary.skills_id;
+FROM jobs_popularity 
+INNER JOIN jobs_salaries ON jobs_salaries.skill_id = jobs_popularity.skill_id
+WHERE demand_count > 10
+ORDER BY avg_salary DESC, demand_count DESC
+LIMIT 10; 
